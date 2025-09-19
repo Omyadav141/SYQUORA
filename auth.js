db3544c4d273ca2a964221c81d955939b589d8ee
Om G. Yadav <omyadav9860@gmail.com>
2025-09-19 15:56:23 +0530

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,36 @@
+import express from "express";
+import fetch from "node-fetch";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+const CLIENT_ID = "804630636275-tt8nkmdugf3sj76uuk98kap07c205o8f.apps.googleusercontent.com";
+const CLIENT_SECRET = "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";
+
+app.post("/verify-token", async (req, res) => {
+  const token = req.body.token;
+
+  try {
+    // Exchange token with Google
+    const googleRes = await fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${token}`);
+    const data = await googleRes.json();
+
+    if (data.aud !== CLIENT_ID) {
+      return res.json({ success: false, message: "Invalid Client ID" });
+    }
+
+    res.json({
+      success: true,
+      user: {
+        name: data.name,
+        email: data.email,
+        picture: data.picture
+      }
+    });
+  } catch (err) {
+    res.json({ success: false, message: "Verification failed" });
+  }
+});
+
+app.listen(5000, () => console.log("Server running on http://localhost:5000"));
